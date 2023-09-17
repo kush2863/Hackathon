29f20d2103a2be5aefde99b58bebd7be2fbc4328
kush2863 <70647570+kush2863@users.noreply.github.com>
2023-09-17 17:56:20 +0530

    added output for the AI response

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -1,17 +1,23 @@
-
 import "./chatbot.css";
-
-
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
- 
-
 import React, { useState } from 'react';
+
+// Define a styled component for the AI response
+function AIResponse({ response }) {
+  return (
+    <div className="ai-response">
+      <h2>AI Response:</h2>
+      <p>{response}</p>
+    </div>
+  );
+}
 
 function Chatbot() {
   const [chat, setChat] = useState([]);
   const [text, setText] = useState('');
   const [file, setFile] = useState(null);
+  const [response, setResponse] = useState('');
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -22,21 +28,30 @@ function Chatbot() {
 
     setText('');
     setFile(null);
+    // Simulate API call (replace with actual API call)
+    simulateAPICall(text, file);
   };
 
-  
-  
-    const handleFileClick = () => {
-      // Trigger a click event on the hidden file input element
-      document.getElementById('fileInput').click();
-    };
-  
-    const handleFileChange = (e) => {
-      // Handle file selection here
-      const selectedFile = e.target.files[0];
-      // Do something with the selected file
-      console.log('Selected file:', selectedFile);
-    };
+  // Simulate an API call
+  const simulateAPICall = (text, file) => {
+    setTimeout(() => {
+      // Replace this with your actual API call logic
+      const aiResponse = 'AI generated response goes here.';
+      setResponse(aiResponse);
+    }, 1500); // Simulate a delay
+  };
+
+  const handleFileClick = () => {
+    // Trigger a click event on the hidden file input element
+    document.getElementById('fileInput').click();
+  };
+
+  const handleFileChange = (e) => {
+    // Handle file selection here
+    const selectedFile = e.target.files[0];
+    // Do something with the selected file
+    console.log('Selected file:', selectedFile);
+  };
 
   return (
     <div className="App">
@@ -69,29 +84,40 @@ function Chatbot() {
           />
           <div className="file">
             <Stack direction="row" spacing={2}>
-      <Button onClick={handleFileClick} variant="contained" component="label" className="file">
-        Choose File
-        <input  
-          id="fileInput"
-          type="file"
-          accept=".txt,.pdf,.png,.jpg,.jpeg,.gif"
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-        />
-      </Button>
-    </Stack>
-    </div>
-    <div className="send"><Stack direction="row" spacing={2}>
-      <Button type="submit" variant="contained" component="label" className="file">
-          Send
-          </Button>
-    </Stack></div>
-    
-    <div/>
+              <Button
+                onClick={handleFileClick}
+                variant="contained"
+                component="label"
+                className="file"
+              >
+                Choose File
+                <input
+                  id="fileInput"
+                  type="file"
+                  accept=".txt,.pdf,.png,.jpg,.jpeg,.gif"
+                  style={{ display: 'none' }}
+                  onChange={handleFileChange}
+                />
+              </Button>
+            </Stack>
+          </div>
+          <div className="send">
+            <Stack direction="row" spacing={2}>
+              <Button
+                type="submit"
+                variant="contained"
+                component="label"
+                className="file"
+              >
+                Send
+              </Button>
+            </Stack>
+          </div>
+          {response && <AIResponse response={response} />}
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Chatbot;
